3e7fa8fc062f6dbcdb2f4e96f3b381da458f232d
Sean McQuillan <seanrmcquillan@gmail.com>
2026-03-14 17:38:34 -0700

    Freezer attribute tests.

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,14 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  let(:freezer) { Freezer.new(100)}
+
+  it 'has a capacity attribute' do
+    expect(freezer.capacity).to eq(100)
+  end
+
+  it 'has a temperature attribute' do
+    expect(freezer.temperature).to eq(70)
+  end
 
 end
