a81c196bcc590aba5c8b3b52a11e741b03a45361
Sean McQuillan <seanrmcquillan@gmail.com>
2026-03-14 17:31:42 -0700

    Water reservoir tests implemented.

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -11,4 +11,24 @@ describe 'A water reservoir' do
     expect(reservoir.current_water_volume).to eq(0)
   end
 
+  it 'is empty' do
+    expect(reservoir).to be_empty
+  end
+
+  it 'can fill the reservoir with water' do
+    reservoir.fill
+    expect(reservoir.current_water_volume).to eq(reservoir.capacity)
+  end
+
+  it 'can drain the reservoir' do
+    reservoir.fill
+    reservoir.drain(5)
+    expect(reservoir.current_water_volume).to eq(5)
+  end
+
+  it 'can not be empty' do
+    reservoir.fill
+    expect(reservoir).not_to be_empty
+  end
+
 end
